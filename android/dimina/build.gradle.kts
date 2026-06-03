b2e4168ffba3657ac7afec614eacdf6133c543d2
Jovkchan <lovewife@vip.qq.com>
2026-06-03 09:04:52 +0800

    fix: add missing camera-camera2 dependency for CameraX

--- android/dimina/build.gradle.kts
+++ android/dimina/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
 
     // CameraX
     implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
 
